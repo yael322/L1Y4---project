98fefad79d2285bfe8569262553aa3d026d51bac
Yael <yaelili322@gmail.com>
2025-12-13 14:18:30 +0200

    Update logic.py

--- logic.py
+++ logic.py
@@ -1,4 +1,5 @@
 from random import randint
+from datetime import datetime, timedelta
 import requests
 
 class Pokemon:
@@ -14,6 +15,8 @@ class Pokemon:
 
         self.power = randint(30, 60)
         self.hp = randint(200, 400)
+
+        self.last_feed_time = datetime.now()
 
         Pokemon.pokemons[pokemon_trainer] = self
 
@@ -62,6 +65,16 @@ class Pokemon:
             enemy.hp = 0
             return f"Победа @{self.pokemon_trainer} над @{enemy.pokemon_trainer}! "
         
+    def feed(self, feed_interval = 20, hp_increase = 10 ):
+        current_time = datetime.now() 
+        delta_time = timedelta(seconds=feed_interval)  
+        if (current_time - self.last_feed_time) > delta_time:
+                self.hp += hp_increase
+                self.last_feed_time = current_time
+                return f"Здоровье покемона увеличено. Текущее здоровье: {self.hp}"
+        else:
+                return f"Следующее время кормления покемона: {self.last_feed_time+delta_time}"
+        
 class Wizard(Pokemon):
     def info(self):
         return 'У тебя покеомон-волшебник\n\n' + super().info()
@@ -87,6 +100,7 @@ if __name__ == '__main__':
     print()
     print(fighter.attack(wizard))
             
+
 
 
 
